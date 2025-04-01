93da5e355b86b223858a221fd79018424c5205ee
JXSS10 <mrhetlr98@gmail.com>
2025-04-01 15:13:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Legend Sources!'
+def home():
+    return """
+<center> 
+    <img src="https://i.giphy.com/media/3o7abAHdYvZdBNnGZq/giphy.webp" style="border-radius: 12px;"/> 
+</center> 
+<style>
+    body { 
+        background: antiquewhite;
+    }
+</style>"""
 
-if __name__ == '__main__':
-    # Run the Flask app on port 80
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
